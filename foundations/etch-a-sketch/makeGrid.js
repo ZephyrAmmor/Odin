a452632ed4354388ae9236f133dd683d889d6440
ZephyrAmmor <amorzephyr@gmail.com>
2025-08-11 17:03:44 +0500

    Update color of cell on each mouseover for 10 times , use rgb

--- foundations/etch-a-sketch/makeGrid.js
+++ foundations/etch-a-sketch/makeGrid.js
@@ -8,8 +8,22 @@ for(let i = 0; i < gridSize; i++){
     }
 }
 
-const cells = document.querySelectorAll('.cells')
 
+
+const cells = document.querySelectorAll('.cell')
 cells.forEach(cell => {
-    cell.addEventListener('dblclick', addStyles())
+    let randomRed = Math.floor(Math.random() * 256)
+    let randomGreen = Math.floor(Math.random() * 256)   
+    let randomBlue = Math.floor(Math.random() * 256)
+    cell.dataset.alpha = 0.10
+    cell.addEventListener('mouseover', (e) => {
+        if(cell.style.background == 'white'){
+            e.target.style.background = `rgba(${randomRed},${randomGreen}, ${randomBlue}, ${parseFloat(cell.dataset.alpha)})`
+        }
+        else{
+            let newAlpha = parseFloat(cell.dataset.alpha) + 0.10
+            cell.dataset.alpha = newAlpha
+            e.target.style.background = `rgba(${randomRed},${randomGreen}, ${randomBlue}, ${parseFloat(cell.dataset.alpha)})`
+        }
+    })
 });
