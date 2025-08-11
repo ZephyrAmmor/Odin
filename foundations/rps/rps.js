49c5b8a1dfa444a269727162dfa62a2378cd27ec
ZephyrAmmor <amorzephyr@gmail.com>
2025-08-11 15:32:00 +0500

    Create global varialbes to keep track of the information needed for multiple functions

--- foundations/rps/rps.js
+++ foundations/rps/rps.js
@@ -4,3 +4,7 @@ const roundPara = document.querySelector('#round')
 const finalResult = document.querySelector('.finalResult')
 const winner = document.querySelector('.winner')
 const totalRounds = document.getElementById('totalRounds')
+
+let totalRoundsToPlay = 5
+let humanVictories = 0
+let computerVictories = 0
