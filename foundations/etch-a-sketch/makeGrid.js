ef924e96fbb3bf1fd6f39f75606610e5eb92869b
ZephyrAmmor <amorzephyr@gmail.com>
2025-08-11 16:24:08 +0500

    Create cell and then add them to contianer

--- foundations/etch-a-sketch/makeGrid.js
+++ foundations/etch-a-sketch/makeGrid.js
@@ -1,2 +1,9 @@
 const container = document.querySelector('.container')
-
+const gridSize = 16
+for(let i = 0; i < gridSize; i++){
+    for(let j = 0; j < gridSize; j ++){
+        const aDiv = document.createElement('div')
+        aDiv.classList.add('cell')
+        container.appendChild(aDiv)
+    }
+}
