b387c4395a1a92558a05bdbc7919b135e32b223c
ZephyrAmmor <amorzephyr@gmail.com>
2025-08-18 18:55:39 +0500

    change the layout strucutre from table to card

--- javascript/library/library.js
+++ javascript/library/library.js
@@ -1,8 +1,6 @@
 let library = []
-const table = document.querySelector('.table')
-const tBody = document.createElement('tbody')
-tBody.setAttribute('id', 'tbody')
-displayTable()
+const container = document.querySelector('.container')
+
 // Open dialog box on add new book click
 const addNewBook = document.getElementById('add-new-book')
 const dialog = document.querySelector('dialog')
@@ -49,32 +47,34 @@ function addNewBookToLibrary(title, author, pages, read){
 }
 function displayBook(newBook){
    
-    const tableRow = document.createElement('tr')
-    tableRow.setAttribute('id', newBook.id)
+    const card = document.createElement('div')
+    card.classList.add('card')
+    card.setAttribute('id', newBook.id)
     for(let key in newBook){
-            const tableData = document.createElement('td')
+            const item = document.createElement('div')
+            item.classList.add('item')
             if(key == 'read'){
                 const readToggleBtn = document.createElement('button')
                 readToggleBtn.textContent = newBook.showRead()
                 readToggleBtn.classList.add('read-status')
                 readToggleBtn.dataset.bookId = newBook.id
-                tableData.appendChild(readToggleBtn)
-                tableRow.appendChild(tableData)
+                item.appendChild(readToggleBtn)
+                card.appendChild(item)
                 continue;
             }
-            tableData.textContent = `${newBook[key]}`
-            tableRow.appendChild(tableData)
+            item.textContent = `${newBook[key]}`
+            card.appendChild(item)
         }
         const delBtn = document.createElement('button')
         delBtn.textContent = 'Del'
         delBtn.classList.add('del')
         delBtn.dataset.bookId = newBook.id
 
-        const delData = document.createElement('td')
+        const delData = document.createElement('div')
         delData.appendChild(delBtn)
 
-        tableRow.appendChild(delData)
-        tbody.appendChild(tableRow)
+        card.appendChild(delData)
+        container.appendChild(card)
     
 }
 
@@ -92,31 +92,8 @@ form.addEventListener('submit', (event) =>{
 
 //Get Books from Library and Display them
 
-function displayTable(){
-    
-    table.textContent = ''
-    const headerRow = document.createElement('thead')
-    const titleHeader = document.createElement('th')
-    const authorHeader = document.createElement('th')
-    const pagesHeader = document.createElement('th')
-    const readHeader = document.createElement('th')
-    const delHeader = document.createElement('th')
 
-    titleHeader.textContent = 'Title'
-    authorHeader.textContent = 'Author'
-    pagesHeader.textContent = 'Pages'
-    readHeader.textContent = 'Read'
-    delHeader.textContent = 'Delete'
-    headerRow.appendChild(titleHeader)
-    headerRow.appendChild(authorHeader)
-    headerRow.appendChild(pagesHeader)
-    headerRow.appendChild(readHeader)
-    headerRow.appendChild(delHeader)
-    table.appendChild(headerRow)
-    table.appendChild(tBody)
-}
-
-table.addEventListener('click', (e) =>{
+container.addEventListener('click', (e) =>{
     if(e.target.classList.contains('read-status')){
         const bookToToggle = library.filter(item => item.id === e.target.dataset.bookId)
         bookToToggle[0].toggle()
@@ -126,6 +103,6 @@ table.addEventListener('click', (e) =>{
         const indexOfBook = library.findIndex(item => item.id === e.target.dataset.bookId)
         library.splice(indexOfBook, 1)
         const rowToDelete = document.getElementById(e.target.dataset.bookId)
-        tbody.removeChild(rowToDelete)
+        container.removeChild(rowToDelete)
     }
 })
