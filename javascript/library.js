14e746d55a007be23bf59bc5a480b6d76310d69b
ZephyrAmmor <amorzephyr@gmail.com>
2025-08-18 11:13:05 +0500

    Add event listener to form, prvent default action and get form data

--- javascript/library.js
+++ javascript/library.js
@@ -1,3 +1,5 @@
+let library = []
+
 // Open dialog box on add new book click
 const addNewBook = document.getElementById('add-new-book')
 const dialog = document.querySelector('dialog')
@@ -11,3 +13,12 @@ function Book(title, author, pages, read){
     this.pages = pages, 
     this.read =read
 }
+
+const form = document.querySelector('form')
+form.addEventListener('submit', (event) =>{
+    event.preventDefault()
+
+    const form = document.getElementsByTagName('form')
+    const formData = new FormData(form)
+    addNewBookToLibrary(...formData)
+})
