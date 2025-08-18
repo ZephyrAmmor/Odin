6e319dc9481940dd7bd073095cb5a4724f0d4ccb
ZephyrAmmor <amorzephyr@gmail.com>
2025-08-18 11:08:45 +0500

    create Book constructor

--- javascript/library.js
+++ javascript/library.js
@@ -1,5 +1,13 @@
+// Open dialog box on add new book click
 const addNewBook = document.getElementById('add-new-book')
 const dialog = document.querySelector('dialog')
 addNewBook.addEventListener('click',() =>{
     dialog.show()
 })
+
+function Book(title, author, pages, read){
+    this.title = title,
+    this.author = author,
+    this.pages = pages, 
+    this.read =read
+}
