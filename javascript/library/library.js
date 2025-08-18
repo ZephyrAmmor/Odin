5a8b23db757c79f27b7b5ce92a4bd88ad627bd90
ZephyrAmmor <amorzephyr@gmail.com>
2025-08-18 19:43:57 +0500

    Add trash svg:

--- javascript/library/library.js
+++ javascript/library/library.js
@@ -80,7 +80,8 @@ function displayBook(newBook){
                 readToggleBtn.textContent = newBook.showRead()
                 readToggleBtn.classList.add('read-status')
                 readToggleBtn.dataset.bookId = newBook.id
-                item.appendChild(readToggleBtn)
+                values.appendChild(readToggleBtn)
+                info.appendChild(values)
                 card.appendChild(item)
                 continue;
             }
@@ -90,7 +91,7 @@ function displayBook(newBook){
             card.appendChild(info)
         }
         const delBtn = document.createElement('button')
-        delBtn.textContent = 'Del'
+        delBtn.innerHTML = '<svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#EA3323"><path d="M280-120q-33 0-56.5-23.5T200-200v-520h-40v-80h200v-40h240v40h200v80h-40v520q0 33-23.5 56.5T680-120H280Zm400-600H280v520h400v-520ZM360-280h80v-360h-80v360Zm160 0h80v-360h-80v360ZM280-720v520-520Z"/></svg>'
         delBtn.classList.add('del')
         delBtn.dataset.bookId = newBook.id
 
