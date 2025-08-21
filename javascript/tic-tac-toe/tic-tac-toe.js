d6dc60bcba59719f017fe0dd901a412e66276014
ZephyrAmmor <amorzephyr@gmail.com>
2025-08-21 10:17:38 +0500

    Hard code players and board and define a function for declaring winner

--- javascript/tic-tac-toe/tic-tac-toe.js
+++ javascript/tic-tac-toe/tic-tac-toe.js
@@ -1,5 +1,5 @@
 let gameBoard = (function(){
-    let board = []
+    let board = ['X','O','X','O','O','X','X','O']
 
     function addMove(index, symbol){
         const indexInt = parseInt(index)
@@ -23,4 +23,21 @@ const players = {}
 
 function creatPlayer(name, symbol){
     return {name, symbol}
+}
+
+players.one = creatPlayer('Ali', 'X')
+players.two = creatPlayer('Arif', 'O')
+
+function winner(){
+    const winnerCombos = [
+        [0,1,2], [3,4,5], [6,7,8],
+        [0,3,6], [1,4,7], [2,5,8],
+        [0,4,8], [2,4,6]
+    ]
+
+    const arr = gameBoard.show()
+    for(let [a,b,c] of winnerCombos){
+        if(arr[a] && arr[a] ===  arr[b] && arr[a] === arr[c])
+            return players.one.symbol == arr[a] ? players.one.name : players.two.name
+    }
 }
