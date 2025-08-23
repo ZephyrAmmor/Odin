6fd5dadde29525819e9f3729faaa9b46fb707f1e
ZephyrAmmor <amorzephyr@gmail.com>
2025-08-23 13:07:36 +0500

    Create index.js and add the logic for handeling tabs, import four js files

---
+++ javascript/restaurant/src/index.js
@@ -0,0 +1,35 @@
+import {home} from './home.js'
+import {menu} from './menu.js'
+import {about} from './about.js'
+import {contact} from './contact.js'
+
+const content = document.querySelector('#content')
+
+content.appendChild(home)
+
+const nav = document.querySelector('nav')
+
+nav.addEventListener('click', (e) =>{
+    while (content.firstChild){
+        content.removeChild(content.firstChild)
+    }
+    const btnId = e.target.id
+    if(btnId === 'home'){
+        content.appendChild(home)
+        return
+    }
+    else if(btnId === 'menu'){
+        content.appendChild(menu)
+        return
+    }
+    else if(btnId === 'about'){
+        content.appendChild(about)
+        return
+    }
+    else if(btnId === 'contact'){
+        content.appendChild(contact)
+        return
+    }
+    else
+        throw Error
+})
