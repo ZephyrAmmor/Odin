eddf14663fc263a74632fae97cdf58f4a5cc0d49
ZephyrAmmor <amorzephyr@gmail.com>
2025-08-23 10:26:05 +0500

    install css-loader and style-loader and them to webpack config

--- javascript/restaurant/webpack.config.js
+++ javascript/restaurant/webpack.config.js
@@ -9,9 +9,19 @@ module.exports = {
         path : path.resolve(__dirname, 'dist'),
         clean: true,
     },
+
     plugins : [
         new HtmlWebpackPlugin({
             template : './src/template.html'
         }),
     ],
+
+    module : {
+        rules : [
+            {
+                test : /\.css$/i,
+                use : ['style-loader, css-loader'],
+            },
+        ],
+    },
 };
