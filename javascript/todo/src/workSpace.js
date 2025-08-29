64ccdd21f194c8e75752be5b5ba53b8355ce482f
ZephyrAmmor <amorzephyr@gmail.com>
2025-08-29 14:41:38 +0500

    Add dummy data function

--- javascript/todo/src/workSpace.js
+++ javascript/todo/src/workSpace.js
@@ -1,4 +1,4 @@
-import {Board} from './constructs.js'
+import {Board, Task, Project} from './constructs.js'
 const workSpace = {
     // Hard Coded id to keep it same no matter what to make local data valid
     id : 'bace00bc-b0a3-46c5-85cb-840be7907ce9',
@@ -43,7 +43,7 @@ const workSpace = {
         for(let [boardId, boardObj] of Object.entries(this.boards)){
             boards[boardId] = boardObj.toJSON()
         }
-        return jsonObj
+        return boards
     },
 
     fromJSON(boards){
@@ -61,8 +61,24 @@ function addToLocalStorage(jsonObj){
 
 function getFromLocalStorage(){
     const rawData = localStorage.getItem('boards')
-    const boards = rawData ? JSON.parse(rawData) : {}
+    const boards = rawData ? JSON.parse(rawData) : getDummyData()
     workSpace.fromJSON(boards)
 }
 
+function getDummyData(){
+    const task1 = new Task('Todo', 'A simple todo list webapp', 'Important', 'Very Urgent', '08/30/25', 'We have to learn what we are supposed to while doing this Task')
+    const task2 = new Task('Tic Tac Toe', 'A front end based tic tac toe game', 'Important', 'Very Urgent', '08/24/25', 'Applying knowledge of objects, factories and IIFE')
+
+    task2.complete = true
+
+    const project = new Project('Odin JS', 'Goal is learing JS to good level by completing JS section of Odin project', 'Very Important', 'Trivial', '08/20/25', '09/15/2025', 'Learning is the goal not just completing it.')
+
+    const board = new Board('Odin Board','A board for keeping all oding Projects in One place', 'Very Important')
+
+    workSpace.addBoard(board)
+    board.addProject(project)
+    project.addTask(task1)
+    project.addTask(task2)
+    
+}   
 export {getFromLocalStorage, workSpace}
