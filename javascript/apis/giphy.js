e1ca118c8e02300145f65a9488d459d68ca33537
ZephyrAmmor <amorzephyr@gmail.com>
2025-09-11 15:17:46 +0500

    Change promises syntax to async await

--- javascript/apis/giphy.js
+++ javascript/apis/giphy.js
@@ -4,38 +4,19 @@ const searchBtn = document.querySelector('#search-btn')
 const newGif = document.querySelector('#new-gif')
 const error = document.querySelector('.error')
 let value = '404';
-fetch('https://api.giphy.com/v1/gifs/translate?api_key=Ak5tiNQ1k7pQgPKCcTmmn4kvrxJ0t9oM&s=cat')
-    .then(function(response) {
-        return (response.json())
-    })
-    .then(function(response) {
-        image.src = response.data.images.original.url
-    })
-searchBtn.addEventListener('click', () =>{
-    value = search.value || '404'
-    fetch(`https://api.giphy.com/v1/gifs/translate?api_key=Ak5tiNQ1k7pQgPKCcTmmn4kvrxJ0t9oM&s=${value}`)
-      .then(function(response){
-        return response.json()
-      })
-      .then(function(response){
-        if (response.data.images.original.url) {
-        image.src = response.data.images.original.url
-        } else showError()
-      })
-      .catch(function(err) {
-        error.textContent(`An Error occuered while fetching this data : ${err}`)
-      })
-})
 
-newGif.addEventListener('click',() =>{
-  fetch(`https://api.giphy.com/v1/gifs/translate?api_key=Ak5tiNQ1k7pQgPKCcTmmn4kvrxJ0t9oM&s=${value}`)
-    .then (function(response) {
-        return response.json()
-    })
-    .then (function(response) {
-        image.src = response.data.images.original.url
-    })
-} )
+async function getGif() {
+  value = search.value || '404'
+    const response = await fetch(`https://api.giphy.com/v1/gifs/translate?api_key=Ak5tiNQ1k7pQgPKCcTmmn4kvrxJ0t9oM&s=${value}`)
+      const gif = await response.json()
+      if (gif.data.images.original.url) {
+        image.src = gif.data.images.original.url
+      } else showError()
+}
+getGif()
+searchBtn.addEventListener('click', getGif)
+
+newGif.addEventListener('click',getGif)
 function showError() {
     error.textContent('Something Goes wrong, or you are out of API calls')
 }
