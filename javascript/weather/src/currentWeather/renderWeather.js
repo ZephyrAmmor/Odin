3a28753779275c09bb36f57e085ef2ea39d7dec9
ZephyrAmmor <amorzephyr@gmail.com>
2025-09-20 09:05:38 +0500

    Modify createcard calls and add title to each card that requires it

--- javascript/weather/src/currentWeather/renderWeather.js
+++ javascript/weather/src/currentWeather/renderWeather.js
@@ -15,13 +15,20 @@ function renderWeather(data) {
   clearElement(container);
 
   const today = new Date();
-  const date = today.getDate();
-  const month = today.getMonth();
+  const date = today.getDate() < 10 ? `0${today.getDate()}` : today.getDate();
+  const month =
+    today.getMonth() < 10 ? `0${today.getMonth()}` : today.getMonth();
   const year = today.getFullYear();
   const dateValue = `${date} : ${month} : ${year}`;
-  const dateCard = createCard('date', 'date-card', '', dateValue);
-  const time = `${today.getHours()} : ${today.getMinutes()} : ${today.getSeconds()}`;
-  const timeCard = createCard('time', 'time-card', '', time);
+  const dateCard = createCard('date', 'date-card', 'Date', '', dateValue);
+  const hours =
+    today.getHours() < 10 ? `0${today.getHours()}` : today.getHours();
+  const minutes =
+    today.getMinutes() < 10 ? `0${today.getMinutes()}` : today.getMinutes();
+  const seconds =
+    today.getSeconds() < 10 ? `0${today.getSeconds()}` : today.getSeconds();
+  const time = `${hours} : ${minutes} : ${seconds}`;
+  const timeCard = createCard('time', 'time-card', 'Time', '', time);
 
   const { days, currentConditions } = data;
   const {
@@ -50,89 +57,138 @@ function renderWeather(data) {
     precipprob,
   } = days[0];
 
-  const tempCard = createCard('temp', 'temp-card', tempUnit, temp);
-  const uvindexCard = createCard('uvindex', 'uvindex-card', uvUnit, uvindex);
+  const tempCard = createCard('temp', 'temp-card', 'Temp', tempUnit, temp);
+  const uvindexCard = createCard(
+    'uvindex',
+    'uvindex-card',
+    'UV Index',
+    uvUnit,
+    uvindex,
+  );
   const feelsLikeCard = createCard(
     'feelslike',
     'feelslike-card',
+    'Feels Like',
     tempUnit,
     feelslike,
   );
-  const precipCard = createCard('precip', 'precip-card', percentUnit, precip);
+  const precipCard = createCard(
+    'precip',
+    'precip-card',
+    'Rain or Snow',
+    percentUnit,
+    precip,
+  );
   const windspeedCard = createCard(
     'windspeed',
     'windspeed-card',
+    'Wind Speed',
     lengthUnit,
     windspeed,
   );
   const winddirCard = createCard(
     'winddir',
     'winddir-card',
+    'Wind Direction',
     '',
     getWindDirection(winddir),
   );
   const conditionsCard = createCard(
     'conditions',
     'conditions-card',
+    'Conditions',
     '',
     conditions,
   );
   const snowdepthCard = createCard(
     'snowdepth',
     'snowdepth-card',
+    'Snow Depth',
     snowdepthUnit,
     snowdepth,
   );
   const cloudcoverCard = createCard(
     'cloudcover',
     'cloudcover-card',
+    'Cloud Cover',
     percentUnit,
     cloudcover,
   );
   const humidityCard = createCard(
     'humidity',
     'humidity-card',
+    'Humidity',
     percentUnit,
     humidity,
   );
-  const dewCard = createCard('dew', 'dew-card', dewUnit, dew);
-  const tempmaxCard = createCard('tempmax', 'tempmax-card', tempUnit, tempmax);
-  const tempminCard = createCard('tempmin', 'tempmin-card', tempUnit, tempmin);
+  const dewCard = createCard('dew', 'dew-card', 'Dew', dewUnit, dew);
+  const tempmaxCard = createCard(
+    'tempmax',
+    'tempmax-card',
+    'Max Temp',
+    tempUnit,
+    tempmax,
+  );
+  const tempminCard = createCard(
+    'tempmin',
+    'tempmin-card',
+    'Min Temp',
+    tempUnit,
+    tempmin,
+  );
   const feelslikemaxCard = createCard(
     'feelslikemax',
     'feelslikemax-card',
+    'Max Feels Like',
     tempUnit,
     feelslikemax,
   );
   const feelslikeminCard = createCard(
     'feelslikemin',
     'feelslikemin-card',
+    'Min Feels Like',
     tempUnit,
     feelslikemin,
   );
   const windgustCard = createCard(
     'windgust',
     'windgust-card',
+    'Wind Gust',
     lengthUnit,
     windgust,
   );
   const visibilityCard = createCard(
     'visibility',
     'visibility-card',
+    'Visibility',
     lengthUnit,
     visibility,
   );
-  const sunriseCard = createCard('sunrise', 'sunrise-card', timeUnit, sunrise);
-  const sunsetCard = createCard('sunset', 'sunset-card', timeUnit, sunset);
+  const sunriseCard = createCard(
+    'sunrise',
+    'sunrise-card',
+    'Sunrise',
+    timeUnit,
+    sunrise,
+  );
+  const sunsetCard = createCard(
+    'sunset',
+    'sunset-card',
+    'Sunset',
+    timeUnit,
+    sunset,
+  );
   const descriptionCard = createCard(
     'description',
     'description-card',
+    'Description',
     '',
     description,
   );
   const precipprobCard = createCard(
     'precipprob',
     'precipprop-card',
+    'Rain or Snow Chances',
     percentUnit,
     precipprob,
   );
@@ -143,11 +199,12 @@ function renderWeather(data) {
     'minmax-feelslike-card',
     '',
     '',
+    '',
   );
-  const minMaxTemp = createCard('minmax-temp', 'minmax-temp-card', '', '');
-  const windCard = createCard('wind', 'wind-card', '', '');
-  const cloudsCard = createCard('clouds', 'clouds-card', '', '');
-  const moistureCard = createCard('moisture', 'moisture-card', '', '');
+  const minMaxTemp = createCard('minmax-temp', 'minmax-temp-card', '', '', '');
+  const windCard = createCard('wind', 'wind-card', '', '', '');
+  const cloudsCard = createCard('clouds', 'clouds-card', '', '', '');
+  const moistureCard = createCard('moisture', 'moisture-card', '', '', '');
   const additionalInfoCard = createCard(
     'additional-info',
     'additional-info-card',
@@ -155,8 +212,8 @@ function renderWeather(data) {
     '',
   );
 
-  const dateTimeCard = createCard('date-time', 'date-time-card', '', '');
-  const sunCard = createCard('sun', 'sun-card', '', '');
+  const dateTimeCard = createCard('date-time', 'date-time-card', '', '', '');
+  const sunCard = createCard('sun', 'sun-card', '', '', '');
 
   minMaxFeelsLike.append(feelslikeminCard, feelslikemaxCard);
   minMaxTemp.append(tempminCard, tempmaxCard);
@@ -170,10 +227,22 @@ function renderWeather(data) {
 
   tempFullCard.append(tempCard, minMaxTemp, feelsLikeCard, minMaxFeelsLike);
 
-  const leftSidebar = createCard('left-sidebar', 'left-sidebar-card', '', '');
+  const leftSidebar = createCard(
+    'left-sidebar',
+    'left-sidebar-card',
+    '',
+    '',
+    '',
+  );
   leftSidebar.append(dateTimeCard, sunCard, conditionsCard);
 
-  const centralView = createCard('central-view', 'central-view-card', '', '');
+  const centralView = createCard(
+    'central-view',
+    'central-view-card',
+    '',
+    '',
+    '',
+  );
   centralView.append(
     tempFullCard,
     windCard,
