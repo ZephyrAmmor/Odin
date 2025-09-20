b999add240a318ac6a2220aa542454f065b445df
ZephyrAmmor <amorzephyr@gmail.com>
2025-09-20 09:06:13 +0500

    Modify CreateCard function to accept a lable arg and return a card containg value and title

--- javascript/weather/src/helpers/components.js
+++ javascript/weather/src/helpers/components.js
@@ -6,11 +6,16 @@ function clearElement(elem) {
   }
 }
 
-function createCard(classNames, id, unit, value) {
+function createCard(classNames, id, title, unit, value) {
   const card = document.createElement('div');
+  const label = document.createElement('div');
+  const text = document.createElement('div');
+  text.textContent = unit ? `${value} ${unit}` : value;
+  label.classList.add(`label-of${classNames}`);
+  label.textContent = title;
   card.className = `${classNames} card`;
   card.id = id;
-  card.textContent = unit ? `${value} ${unit}` : value;
+  card.append(title, text);
   return card;
 }
 
